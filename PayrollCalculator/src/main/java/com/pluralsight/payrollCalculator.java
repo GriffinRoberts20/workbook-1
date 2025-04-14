f240d361b3413e510c56bde1e186c21b53385bcd
Griffin Roberts <gc20roberts@gmail.com>
2025-04-14 15:21:38 -0500

    added overtime to payroll calculator

--- PayrollCalculator/src/main/java/com/pluralsight/payrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/payrollCalculator.java
@@ -11,7 +11,15 @@ public class payrollCalculator {
         System.out.print("Enter your pay rate: ");
         float payRate=scanner.nextFloat();
         scanner.nextLine();
-        float grossPay=payRate*hours;
-        System.out.printf("%s's total pay for %.2f hours work at $%.2f/hr is $%.2f%n",name,hours,payRate,grossPay);
+        float grossPay=0.f;
+        if (hours>40){
+            grossPay= (float) (payRate*40+payRate*1.5*(hours-40));
+            System.out.printf("%s's total overtime pay for %.2f hours work at $%.2f/hr is $%.2f%n",name,hours,payRate,grossPay);
+        }
+        else {
+            grossPay = payRate * hours;
+            System.out.printf("%s's total pay for %.2f hours work at $%.2f/hr is $%.2f%n",name,hours,payRate,grossPay);
+        }
+
     }
 }
