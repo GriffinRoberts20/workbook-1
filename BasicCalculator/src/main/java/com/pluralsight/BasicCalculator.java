f3f40041a142458615b1021bed9b9da6d550af7b
Griffin Roberts <gc20roberts@gmail.com>
2025-04-14 15:56:08 -0500

    refactored BasicCalculator to move operations to methods

--- BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
+++ BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
@@ -7,6 +7,28 @@ import java.util.Scanner;
 public class BasicCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String action=operationChoice(scanner);
+        if (action.equals("a")) {
+            add(scanner);
+        }
+        else if (action.equals("s")) {
+            subtract(scanner);
+        }
+        else if (action.equals("m")) {
+            multiply(scanner);
+        }
+        else if (action.equals("d")) {
+            divide(scanner);
+        }
+        else if (action.equals("e")) {
+            power(scanner);
+        }
+        else if (action.equals("r")) {
+            root(scanner);
+        }
+
+    }
+    public static String operationChoice(Scanner scanner){
         List<String> options = new ArrayList<>();
         options.add("a");
         options.add("s");
@@ -14,8 +36,8 @@ public class BasicCalculator {
         options.add("d");
         options.add("e");
         options.add("r");
-        boolean f = false;
         String action = "";
+        boolean f=false;
         while (!f) {
             System.out.print("Possible Calculations:\n   (A) Add(x+y)\n   (S) Subtract(x-y)\n   (M) Multiply(x*y)\n   (D) Divide(x/y)\n   (E) Exponent(x^y)\n   (R) Root(x^(1/y))\nChoose calculation: ");
             action = scanner.nextLine().toLowerCase();
@@ -26,75 +48,91 @@ public class BasicCalculator {
                 System.out.println("Error: entered invalid option.");
             }
         }
+        return action;
+    }
+
+    public static void add(Scanner scanner){
         System.out.print("Enter first number: ");
         double x = scanner.nextDouble();
         System.out.print("Enter second number: ");
         double y = scanner.nextDouble();
         scanner.nextLine();
-        if (action.equals("a")) {
-            double sum =add(x,y);
-            System.out.println(x + "+" + y + "=" + sum);
-        }
-        else if (action.equals("s")) {
-            double difference =subtract(x,y);
-            System.out.println(x + "-" + y + "=" + difference);
-        }
-        else if (action.equals("m")) {
-            double product = multiply(x,y);
-            System.out.println(x + "-" + y + "=" + product);
-        }
-        else if (action.equals("d")) {
-            while (y==0){
-                System.out.print("Error: cannot divide by 0. Enter a new second number: ");
-                y=scanner.nextDouble();
-                scanner.nextLine();
-            }
-            double product = divide(x,y);
-            System.out.println(x + "/" + y + "=" + product);
-        }
-        else if (action.equals("e")) {
-            double exponential = power(x,y);
-            System.out.println(x + "^" + y + "=" + exponential);
-        }
-        else if (action.equals("r")) {
-            while (x<0&&y%2==0){
-                System.out.println("Error: cannot take an even root of a negative.");
-                System.out.print("Enter first number: ");
-                x = scanner.nextDouble();
-                System.out.print("Enter second number: ");
-                y = scanner.nextDouble();
-                scanner.nextLine();
-            }
-            while (y==0){
-                System.out.print("Error: cannot divide by 0. Enter a new second number: ");
-                y=scanner.nextDouble();
-                scanner.nextLine();
-            }
-            double root = power(x,1/y);
-            System.out.println(x + "^(1/" + y + ")=" + root);
-        }
-
+        double sum=x+y;
+        System.out.println(x + "+" + y + "=" + sum);
     }
-    public static double add(double x, double y){
-        return x+y;
+    public static void subtract(Scanner scanner){
+        System.out.print("Enter first number: ");
+        double x = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double y = scanner.nextDouble();
+        scanner.nextLine();
+        double difference=x-y;
+        System.out.println(x + "-" + y + "=" + difference);
     }
-    public static double subtract(double x, double y){
-        return x-y;
+    public static void multiply(Scanner scanner){
+        System.out.print("Enter first number: ");
+        double x = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double y = scanner.nextDouble();
+        scanner.nextLine();
+        double product=x*y;
+        System.out.println(x + "-" + y + "=" + product);
     }
-    public static double multiply(double x, double y){
-        return x*y;
+    public static void divide(Scanner scanner){
+        System.out.print("Enter first number: ");
+        double x = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double y = scanner.nextDouble();
+        scanner.nextLine();
+        while (y==0){
+            System.out.print("Error: cannot divide by 0. Enter a new second number: ");
+            y=scanner.nextDouble();
+            scanner.nextLine();
+        }
+        double product = x/y;
+        System.out.println(x + "/" + y + "=" + product);
     }
-    public static double divide(double x, double y){
-        return x/y;
-    }
-    public static double power(double x, double y){
-        double out;
+    public static void power(Scanner scanner){
+        System.out.print("Enter first number: ");
+        double x = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double y = scanner.nextDouble();
+        scanner.nextLine();
+        double exponential;
         if (x>0){
-            out=Math.pow(x,y);
+            exponential=Math.pow(x,y);
         }
         else{
-            out=-Math.pow(-x,y);
+            exponential=-Math.pow(-x,y);
         }
-        return out;
+        System.out.println(x + "^" + y + "=" + exponential);
+    }
+    public static void root(Scanner scanner){
+        System.out.print("Enter first number: ");
+        double x = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double y = scanner.nextDouble();
+        scanner.nextLine();
+        while (x<0&&y%2==0){
+            System.out.println("Error: cannot take an even root of a negative.");
+            System.out.print("Enter first number: ");
+            x = scanner.nextDouble();
+            System.out.print("Enter second number: ");
+            y = scanner.nextDouble();
+            scanner.nextLine();
+        }
+        while (y==0){
+            System.out.print("Error: cannot divide by 0. Enter a new second number: ");
+            y=scanner.nextDouble();
+            scanner.nextLine();
+        }
+        double root;
+        if (x>0){
+            root=Math.pow(x,1/y);
+        }
+        else{
+            root=-Math.pow(-x,1/y);
+        }
+        System.out.println(x + "^(1/" + y + ")=" + root);
     }
 }
